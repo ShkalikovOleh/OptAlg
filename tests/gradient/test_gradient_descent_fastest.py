ce45b5995498d34025a9a31a922d5874a62b6b12
Oleh Shkalikov <Shkalikov.Oleh@outlook.com>
2020-09-14 17:16:38 +0300

    Add gd fastest tests

---
+++ tests/gradient/test_gradient_descent_fastest.py
@@ -0,0 +1,33 @@
+import unittest
+import numpy as np
+from optalg.iterative import SimpleSearch
+from optalg.gradient import GradientDescentFastest
+from optalg.stop_criteria import GradientNormCriteria
+from optalg.stop_criteria import IterationNumberCriteria
+from ..inrange_assertion import InRangeAssertion
+
+
+class GradientDescentFastestTests(unittest.TestCase, InRangeAssertion):
+
+    @staticmethod
+    def f(x):
+        return 5*x**2 - 2*x + 4
+
+    def test_convergence(self):
+        gnCriteria = GradientNormCriteria(10 ** -3)
+        step_opt = SimpleSearch((10**-3, 1), 50)
+
+        opt = GradientDescentFastest(np.array([-3]), gnCriteria, step_opt)
+        x_opt = opt.optimize(self.f)
+
+        self.assertInRange(x_opt, 0.2, 10**-3)
+
+    def test_get_history(self):
+        iteration_count = 10
+        nCriteria = IterationNumberCriteria(iteration_count)
+        step_opt = SimpleSearch((10**-3, 1), 50)
+
+        opt = GradientDescentFastest(np.array([-3]), nCriteria, step_opt)
+        x_opt = opt.optimize(self.f)
+
+        self.assertEqual(iteration_count, len(opt.get_last_history()) - 1)
