a435e79b9fb4eb96e970f23c2eabd8ed939c1062
mkrooted <koreshov.m@gmail.com>
2020-09-22 23:37:10 +0300

    Refactor test - remove functions list (#16)

--- tests/iterative/test_fibonacci.py
+++ tests/iterative/test_fibonacci.py
@@ -8,18 +8,18 @@ class FibonacciTests(unittest.TestCase):
         return x**2 - 4*x + 4
     
     def setUp(self):  
-        # [(f, x_opt)]
-        self.funcs = [(self.f,2.0)]
+        # x_opt - real optimum
+        self.x_opt = 2
         self.precisions = [1,0.5,0.01,0.001,0.0001]
         self.bounds = [-5,5]
 
     def test_fibonacci(self):
         for prec in self.precisions:
-            x = Fibonacci(self.bounds, prec).optimize(self.funcs[0][0])
-            self.assertAlmostEqual(x, self.funcs[0][1], delta=prec)
+            x = Fibonacci(self.bounds, prec).optimize(self.f)
+            self.assertAlmostEqual(x, self.x_opt, delta=prec)
 
 
     def test_modfibonacci(self):
         for prec in self.precisions:
-            x = ModFibonacci(self.bounds, prec).optimize(self.funcs[0][0])
-            self.assertAlmostEqual(x, self.funcs[0][1], delta=prec)
+            x = ModFibonacci(self.bounds, prec).optimize(self.f)
+            self.assertAlmostEqual(x, self.x_opt, delta=prec)
