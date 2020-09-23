ed03ce3fc59c5803ad03d10c7b3c1f723dc659d7
Michael Koreshkov <36203215+mkrooted256@users.noreply.github.com>
2020-09-23 09:57:35 +0300

    Fibonacci Iterative algorithms (#16)
    * Add simple Fibonacci  algo (#13) + tests
    
    * Fix Fibonacci (#13). Modify tests.
    
    * Change readme: add fibonacci
    
    * Add modified fibonacci #13
    
    * Fix readme link to Fibonacci methods
    
    * Rename 'advanced' to 'fibonacci' (#16).
    Merge fix.
    Other minor fixes.
    
    * Merge fix after e8b0189 (again)
    
    * Refactor test - remove functions list (#16)
    
    * Fix Fibonacci location in ReadMe
    
    Co-authored-by: Shkalikov Oleh <Shkalikov.Oleh@outlook.com>

---
+++ optalg/iterative/fibonacci.py
@@ -0,0 +1,117 @@
+import numpy as np
+from ..optimizer import Optimizer
+
+class Fibonacci(Optimizer):
+    """
+    Find argmin of one-dimensional UNIMODAL function in bounds
+
+    Drawback: Algorithm is very sensitive to float errors
+    """
+
+    def __init__(self, bounds, epsilon):
+        """
+        Init iterative search object
+
+        Args:
+            bounds (array-like): bounds of search. Must contain 2 values.
+                                Second value must be larger than first.
+            epsilon (float): calculation precision
+        """
+        super().__init__()
+        self.__a, self.__b = bounds
+        self.__epsilon = epsilon
+
+    def optimize(self, f):
+        diam = self.__b - self.__a
+        F1, F2, F3 = 1, 1, 2
+        j = 1
+        while not (F2 < diam/self.__epsilon <= F3):
+            F1, F2, F3 = F2, F3, F2+F3
+            j = j+1
+        m = j
+        
+        k = diam * F1/F3
+        y = self.__a + k
+        z = self.__b - k
+
+        if f(y) <= f(z):
+            a = self.__a
+            b = z
+        else:
+            a = y
+            b = self.__b
+
+        k = 1
+        while k<m-1:
+            if f(y) <= f(z):
+                z = y
+                y = a + b - y
+            else:
+                y = z
+                z = a + b - z
+                
+            if f(y) <= f(z):
+                b = z
+            else:
+                a = y
+            k=k+1
+        X = (a+b)/2
+        
+        return X
+
+class ModFibonacci(Optimizer):
+    """
+    Find argmin of one-dimensional UNIMODAL function in bounds
+    Modified Fibonacci. More float-error-prone.
+    """
+
+    def __init__(self, bounds, epsilon):
+        """
+        Init iterative search object
+
+        Args:
+            bounds (array-like): bounds of search. Must contain 2 values.
+                                Second value must be larger than first.
+            epsilon (float): calculation precision
+        """
+        super().__init__()
+        self.__a, self.__b = bounds
+        self.__epsilon = epsilon
+
+    def optimize(self, f):
+        diam = self.__b - self.__a
+        F = [1, 1]
+        j = 1
+        F.append(F[j]+F[j-1])
+        while not (F[j] < diam/self.__epsilon <= F[j+1]):
+            F.append(F[j+1]+F[j])
+            j = j+1
+        m = j
+        
+        y = self.__a + diam * F[m-1]/F[m+1]
+        z = self.__a + diam * F[m]/F[m+1]
+
+        if f(y) <= f(z):
+            a = self.__a
+            b = z
+        else:
+            a = y
+            b = self.__b
+
+        k = 1
+        while k<m-1:
+            if f(y) <= f(z):
+                z = y
+                y = a + diam * F[m-k-1]/F[m+1]
+            else:
+                y = z
+                z = a + diam * F[m-k]/F[m+1]
+                
+            if f(y) <= f(z):
+                b = z
+            else:
+                a = y
+            k=k+1
+        X = (a+b)/2
+        
+        return X
