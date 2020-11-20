ac6f08a9dd33ce73e9cba7605d94571eab999948
Oleh Shkalikov <Shkalikov.Oleh@outlook.com>
2020-11-20 16:37:42 +0200

    Add Gray decoder

--- optalg/evolutional/generator/binary.py
+++ optalg/evolutional/generator/binary.py
@@ -4,7 +4,10 @@ from .generator_base import Generator
 
 class BinaryGenerator(Generator):
 
-    def __call__(self, population_size: int, n_variables: int, n_genes: int) -> np.ndarray:
-        shape = (population_size, n_variables, n_genes)
-        size = population_size * n_variables, n_genes
+    def __init__(self, n_genes: int = 22) -> None:
+        super().__init__(n_genes)
+
+    def __call__(self, population_size: int, n_variables: int) -> np.ndarray:
+        shape = (population_size, n_variables, self._n_genes)
+        size = population_size * n_variables * self._n_genes
         return np.random.randint(2, size=size).reshape(shape)
--- optalg/evolutional/generator/generator_base.py
+++ optalg/evolutional/generator/generator_base.py
@@ -1,11 +1,19 @@
 import numpy as np
 from abc import ABC, abstractmethod
 
+from numpy.lib.type_check import real
+
 
 class Generator(ABC):
 
+    def __init__(self, n_genes: int) -> None:
+        if n_genes <= 0:
+            raise ValueError("Number of genes must be grater than 0")
+
+        self._n_genes = n_genes
+
     @abstractmethod
-    def __call__(self, population_size: int, n_variables: int, n_genes: int) -> np.ndarray:
+    def __call__(self, population_size: int, n_variables: int) -> np.ndarray:
         """
         Generate population
         :param population_size: number of individuals in population
